dde3f0e38cb23c9d81c65d52c1c80b3b9ad4a2d2
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-08-02 11:27:56 +0000

    1.11 unicafe: HTML table

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,83 +6,88 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 
 const StatisticsLine = ({text, value}) => {
   return( 
-  <p>{text} {value}</p>
-  )
-}
-
-const Statistics = ({feedbacks}) => {
- const allFeedbacks = Object.values(feedbacks).reduce((a, b) => a + b, 0)
- 
- if (allFeedbacks === 0) {
-  return (
-  <>
-  <p>No feedback given</p>
-  </> 
-  )
- } else {
-  return (
-    <>
-    <StatisticsLine text = "Good" value = {feedbacks["good"]} />
-    <StatisticsLine text = "Neutral" value = {feedbacks["neutral"]} />
-    <StatisticsLine text = "Bad" value = {feedbacks["bad"]} />
-    <StatisticsLine text = "All" value = {allFeedbacks}/>
-    <StatisticsLine text = "Average" value = {
-    ((feedbacks["good"] + (feedbacks["bad"] *(-1)))/
-    allFeedbacks).toFixed(2)
-    } />
-    <StatisticsLine text = "Positive" value =  {((feedbacks["good"] / allFeedbacks )*100).toFixed(2) + "%"} />
-    </>
+    <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+    </tr> 
     )
   }
-}
   
-  
-
-  
-  const App = () => {
-  
-    //single use state for all answers collectively
-    const [feedbacks, setFeedback] = useState({
-      good: 0,
-      neutral: 0,
-      bad: 0
-    })
+  const Statistics = ({feedbacks}) => {
+    const allFeedbacks = Object.values(feedbacks).reduce((a, b) => a + b, 0)
     
-    const pressButton = (type) => () => {
-     // console.log("value before click", feedbacks)
-      setFeedback({
-        ...feedbacks,
-        [type]: feedbacks[type]+1
-      })
-    }
-    
-    // useEffect(() => {
-    //   console.log("value after click/render", feedbacks)
-    // }, [pressButton]) // <-- this effect runs whenever `stateVariable` changes
-    
-    
-    return (
-      <div>
-      <h1>Give feedback</h1>
+    if (allFeedbacks === 0) {
+      return (
+        <>
+        <p>No feedback given</p>
+        </> 
+        )
+      } else {
+        return (
+          <table>
+          <tbody>
+          <StatisticsLine text = "Good" value = {feedbacks["good"]} />
+          <StatisticsLine text = "Neutral" value = {feedbacks["neutral"]} />
+          <StatisticsLine text = "Bad" value = {feedbacks["bad"]} />
+          <StatisticsLine text = "All" value = {allFeedbacks}/>
+          <StatisticsLine text = "Average" value = {
+            ((feedbacks["good"] + (feedbacks["bad"] *(-1)))/
+            allFeedbacks).toFixed(2)
+          } />
+          <StatisticsLine text = "Positive" value =  {((feedbacks["good"] / allFeedbacks )*100).toFixed(2) + "%"} />
+          </tbody>
+          </table>
+          )
+        }
+      }
       
-      <Button 
-      handleClick= {pressButton("good")}
-      text="Good"
-      />
-      <Button 
-      handleClick= {pressButton("neutral")}
-      text="Neutral"
-      />
-      <Button 
-      handleClick= {pressButton("bad")}
-      text="Bad"
-      />
       
-      <h2>Statistics</h2>
-
-      <Statistics feedbacks = {feedbacks}  />
-      </div>
-      )
-    }
-    
-    export default App
+      
+      
+      const App = () => {
+        
+        //single use state for all answers collectively
+        const [feedbacks, setFeedback] = useState({
+          good: 0,
+          neutral: 0,
+          bad: 0
+        })
+        
+        const pressButton = (type) => () => {
+          // console.log("value before click", feedbacks)
+          setFeedback({
+            ...feedbacks,
+            [type]: feedbacks[type]+1
+          })
+        }
+        
+        // useEffect(() => {
+        //   console.log("value after click/render", feedbacks)
+        // }, [pressButton]) // <-- this effect runs whenever `stateVariable` changes
+        
+        
+        return (
+          <div>
+          <h1>Give feedback</h1>
+          
+          <Button 
+          handleClick= {pressButton("good")}
+          text="Good"
+          />
+          <Button 
+          handleClick= {pressButton("neutral")}
+          text="Neutral"
+          />
+          <Button 
+          handleClick= {pressButton("bad")}
+          text="Bad"
+          />
+          
+          <h2>Statistics</h2>
+          
+          <Statistics feedbacks = {feedbacks}  />
+          </div>
+          )
+        }
+        
+        export default App
