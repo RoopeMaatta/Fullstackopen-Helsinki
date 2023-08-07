6cc96a58aeb38196a684c1195763241573f22ee2
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-08-07 08:04:44 +0000

    2.7 phonebook: duplicate name alert message

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,56 +1,65 @@
 import { useState } from 'react'
 
+const normalizeInput = (str) => 
+str.toLowerCase().replace(/\s+/g, ' ').trim()
+
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
-      id: 1 }
+    id: 1 }
   ]) 
   const [newName, setNewName] = useState('')
   const [highId, setHighId] = useState(1)
-
+  
   const addContact = (event) => {
     event.preventDefault() // prevent default form submit logic
-    // console.log("Button clicked", event.target)
-    const contactObject = {
-      name: newName,
-      phoneNumber: "placeholder 3230948203",
-      id: highId+1
+    if (persons.some(person => normalizeInput(person.name) === normalizeInput(newName))) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      // console.log("Button clicked", event.target)
+      const contactObject = {
+        name: newName,
+        phoneNumber: "placeholder 3230948203",
+        id: highId+1
+      }
+      setPersons(persons.concat(contactObject))
+      setHighId(highId+1)
+      setNewName("")
     }
-    setPersons(persons.concat(contactObject))
-    setHighId(highId+1)
-    setNewName("")
   }
-
+  
   const handleContactChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
-
+  
   return (
     <div>
-      <h2>Phonebook</h2>
-      
-      <form onSubmit={addContact}>
-        <div>
-          name: 
-          <input
-          value = {newName}
-          onChange={handleContactChange}
-           />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-
-      <h2>Numbers</h2>
-      ...
-      <ul>{persons.map(person => (<li key={person.id}>{person.name}</li>))}</ul>
-
-      <div>debug: {newName}</div>
-
+    <h2>Phonebook</h2>
+    
+    <form onSubmit={addContact}>
+    <div>
+    name: 
+    <input
+    value = {newName}
+    onChange={handleContactChange}
+    />
     </div>
-  )
-}
-
-export default App
+    <div>
+    <button type="submit">add</button>
+    </div>
+    </form>
+    
+    <h2>Numbers</h2>
+    ...
+    <ul>{persons.map(person => (<li key={person.id}>{person.name}</li>))}</ul>
+    
+    <div>debug: {newName}</div>
+    
+    </div>
+    )
+  }
+  
+  export default App
