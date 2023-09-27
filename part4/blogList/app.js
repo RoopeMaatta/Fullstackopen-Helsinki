e81d4172f53630eef3e9f65d51c837fb0d71fe5b
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-09-27 13:43:10 +0000

    4.2 BlogList📝: middleware module

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -1,10 +1,9 @@
+const config = require('./utils/config')
 const express = require('express')
+const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
-
-const app = express()
-
-const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -21,8 +20,13 @@ mongoose.connect(config.MONGODB_URI)
 
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
