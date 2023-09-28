96845721f53662e3fa5f6d02ace91e1aca8cee67
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-09-28 12:10:44 +0000

    4.4 BlogList📝: filter array missing like property

--- part4/blogList/tests/listHelper.test.js
+++ part4/blogList/tests/listHelper.test.js
@@ -51,6 +51,29 @@ describe("total likes", () => {
   ]
 
 
+  const listWithBlogsMissingValues = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      __v: 0
+    },
+  ]
+
 
 
   test("of empty list is zero", () => {
@@ -66,6 +89,12 @@ describe("total likes", () => {
   test("of a bigger list is calculated right", () => {
     const result = listHelper.totalLikes(listWithMultipleBlogs)
     expect(result).toBe(24)
+  })
+
+
+  test("of a list with missing values / empty objects", () => {
+    const result = listHelper.totalLikes(listWithBlogsMissingValues)
+    expect(result).toBe(12)
   })
 } )
 
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,12 +2,22 @@ const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = (blogs) => {
 
-  return blogs.length === 0
-    ? 0
-    : blogs.map(blog => blog.likes).reduce((a,b) => a+b)
+const totalLikes = (blogs) => {
+  return blogs
+    .filter(blog => blog.hasOwnProperty('likes') && blog.likes !== undefined)
+    .map(blog => blog.likes)
+    .reduce((a, b) => a + b, 0)
 }
+
+
+// // throws error if blogs has a element without likes property
+// const totalLikes = (blogs) => {
+
+//   return blogs.length === 0
+//     ? 0
+//     : blogs.map(blog => blog.likes).reduce((a,b) => a+b)
+// }
 
 
 
