e2f6591a99f12eb01353061d7ec9fed11a226927
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-09-28 12:01:04 +0000

    4.4 BlogList📝: Total likes function testing

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,7 +2,24 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  return blogs.length === 0
+    ? 0
+    : blogs.map(blog => blog.likes).reduce((a,b) => a+b)
+}
+
+
+
+
+
+
+
+
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
 
