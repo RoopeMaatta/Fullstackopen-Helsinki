c468c025121f5b1745c1d933958162a3527e6f80
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-09-28 13:47:21 +0000

    4.5* BlogList📝: favourite Blog function & testing

--- part4/blogList/tests/listHelper.test.js
+++ part4/blogList/tests/listHelper.test.js
@@ -1,5 +1,16 @@
 const listHelper = require('../utils/list_helper')
 
+
+const {
+  emptyList,
+  listWithOneBlog,
+  listWithMultipleBlogs,
+  listWithBlogsMissingValues,
+} = require('./blogsTestingData');
+
+
+
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -8,73 +19,8 @@ test('dummy returns one', () => {
 })
 
 
-
+// Counts total likes of all blog posts
 describe("total likes", () => {
-  const emptyList = []
-
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listWithMultipleBlogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-  ]
-
-
-  const listWithBlogsMissingValues = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      __v: 0
-    },
-  ]
-
-
 
   test("of empty list is zero", () => {
     const result = listHelper.totalLikes(emptyList)
@@ -96,6 +42,51 @@ describe("total likes", () => {
     const result = listHelper.totalLikes(listWithBlogsMissingValues)
     expect(result).toBe(12)
   })
+
+} )
+
+
+// Finds the blog with the most likes
+describe("favourite blog", () => {
+
+  test("of empty list is zero", () => {
+    const result = listHelper.favouriteBlog(emptyList)
+    expect(result).toBe("No blogs found")
+  })
+
+  test("when list has only one blog equals the likes of that", () => {
+    const result = listHelper.favouriteBlog(listWithOneBlog)
+    expect(result).toEqual(
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5
+      }
+    )
+  })
+
+  test("of a bigger list is find and return the blog with most likes", () => {
+    const result = listHelper.favouriteBlog(listWithMultipleBlogs)
+    expect(result).toEqual(
+      {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        likes: 12
+      }
+    )
+  })
+
+  test("of a list with missing values / empty objects", () => {
+    const result = listHelper.favouriteBlog(listWithBlogsMissingValues)
+    expect(result).toEqual(
+      {
+        title: "React patterns",
+        author: "Michael Chan",
+        likes: 7
+      }
+    )
+  })
+
 } )
 
 
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,25 +2,38 @@ const dummy = (blogs) => {
   return 1
 }
 
-
+// Counts total likes of all blog posts
 const totalLikes = (blogs) => {
   return blogs
-    .filter(blog => blog.hasOwnProperty('likes') && blog.likes !== undefined)
+    .filter(blog => blog.hasOwnProperty('likes') && blog.likes !== undefined) // filters away objects that do not have like property
     .map(blog => blog.likes)
     .reduce((a, b) => a + b, 0)
 }
 
 
-// // throws error if blogs has a element without likes property
-// const totalLikes = (blogs) => {
-
-//   return blogs.length === 0
-//     ? 0
-//     : blogs.map(blog => blog.likes).reduce((a,b) => a+b)
-// }
 
 
+// Finds the blog with the most likes
+const favouriteBlog = (blogs) => {
+  const mostLikesOnBlog = Math.max(
+    ...blogs //converts array of numvers into individual numers for math.max
+      .filter(blog => blog.hasOwnProperty('likes') && blog.likes !== undefined)
+      .map(blog => blog.likes)
+  );
 
+  const blogWithMostLikes = blogs
+    .find(blog => blog.likes === mostLikesOnBlog);
+
+  if (!blogWithMostLikes || blogs.length === 0) {
+    return "No blogs found";
+  }
+
+  return {
+    title: blogWithMostLikes.title,
+    author: blogWithMostLikes.author,
+    likes: blogWithMostLikes.likes
+  };
+}
 
 
 
@@ -30,6 +43,7 @@ const totalLikes = (blogs) => {
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
 }
 
