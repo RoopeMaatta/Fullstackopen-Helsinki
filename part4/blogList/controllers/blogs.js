d6fc6eaac8e2f4e31fd9f17f5a6bcfa8273aa0c6
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-10-26 07:58:55 +0000

    4.8 BlogList📝: test http get. aync/await

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,12 +1,9 @@
 const blogsRouter = require("express").Router()
 const Blog = require ("../models/blog")
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs)
 })
 
 blogsRouter.post('/', (request, response) => {
@@ -17,6 +14,13 @@ blogsRouter.post('/', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const blog = new Blog(request.body)
+
+  const result = await blog.save()
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter
---
+++ part4/blogList/tests/blog_api.test.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+// Initial database setup //
+
+const initialBlogs = [
+  {
+    title: 'HTML is easy',
+    author: "Sir Wuffelius",
+    url: "www.Awuuh.com/blogs",
+    likes: "3",
+  },
+  {
+    title: 'Browser can execute only JavaScript',
+    author: "Mister Nekkonen",
+    url: "www.Miumau.fi/bonks",
+    likes: "69",
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+
+// TESTS //
+
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
+
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+
+test('a spesific blog is within returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain('Browser can execute only JavaScript')
+
+
+})
