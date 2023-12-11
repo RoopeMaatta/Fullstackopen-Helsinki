6fb62f9c9a2409fb56efcf1f887ace1ba01b99b6
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-12-11 14:11:22 +0000

    5.13 Frontend: test Blog renders content

--- part5/frontend/src/tests/Blog.test.js
+++ part5/frontend/src/tests/Blog.test.js
@@ -1,2 +1,32 @@
 // test blog component renders blog title and author, but nor url or likes.
 // css classes cna be added if needed.
+
+import Blog from '../components/Blog'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+
+
+describe('<Blog />', () => {
+
+  test('renders content', () => {
+
+    const mockHandleLike = jest.fn()
+    const mockHandleDelete = jest.fn()
+
+    const blog = {
+      title: 'Testing Title',
+      url: 'Testing Url',
+      likes: 12,
+      author: 'Testing Author',
+      id: 1,
+    }
+
+
+    render(<Blog blog={blog}
+      handleLike={mockHandleLike}
+      handleDelete={mockHandleDelete}
+    />)
+    screen.debug()
+  })
+})
