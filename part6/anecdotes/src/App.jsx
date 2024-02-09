275ae83da28b12a9203b8aed2e0cb51b060335ad
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-02-09 13:53:41 +0000

    6.5 anecdotes: sort by votes, useSelector

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -2,7 +2,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import { voteFor, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => {
+    // Sort the anecdotes by number of votes in descending order
+    return [...state].sort((a, b) => b.votes - a.votes)
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -23,6 +26,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+
       {anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
@@ -34,6 +38,7 @@ const App = () => {
           </div>
         </div>
       )}
+
       <h2>create new</h2>
       <form onSubmit={newAnecdote}>
         <div><input name='anecdote' /></div>
