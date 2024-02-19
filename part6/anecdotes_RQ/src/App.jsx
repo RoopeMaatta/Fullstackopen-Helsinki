3e2a2baa558b4fc45346a1f5f2efc65e1c669f56
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-02-19 12:25:39 +0000

    6.21-22 anecdotes: post new and update vote. Rerender instantly

--- part6/anecdotes_RQ/src/App.jsx
+++ part6/anecdotes_RQ/src/App.jsx
@@ -1,19 +1,36 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-import { useQuery } from '@tanstack/react-query' 
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query' 
+import { getAnecdotes, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) =>{
+      queryClient.setQueryData(['anecdotes'], (oldAnecdotes) => {
+        return oldAnecdotes.map((anecdote) =>
+          anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote
+        )
+      })
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes +1 })
+    //console.log(anecdote)
   }
 
+  
   const { isPending, isError, data, error } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
-    retry: 1
+    retry: 1,
+    refetchOnWindowFocus: false
   })
   if ( isPending ) {
     return <div>loading data...</div>
--- part6/anecdotes_RQ/src/requests.js
+++ part6/anecdotes_RQ/src/requests.js
@@ -4,5 +4,11 @@ const baseUrl = 'https://roopemaatta-humble-sniffle-7g7pqv564pwhrpp-3001.app.git
 export const getAnecdotes = () =>
   axios.get(baseUrl).then(res => res.data)
 
+export const createAnecdote = newAnecdote =>
+  axios.post(baseUrl, newAnecdote).then(res => res.data)
+
+export const updateAnecdote = updatedAnecdote =>
+  axios.put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote).then(res => res.data)
+
 // axios.get(`${baseUrl}/${id}`)
   
