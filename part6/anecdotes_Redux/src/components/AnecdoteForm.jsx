d7602aa0dd7d8bc85d58ffac880b6806c5f260c6
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-02-19 11:24:32 +0000

    6.20 anecdotes: setup React Query project version

--- part6/anecdotes_Redux/src/components/AnecdoteForm.jsx
+++ part6/anecdotes_Redux/src/components/AnecdoteForm.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux'
-import { createAnecdote } from './../reducers/anecdoteReducer'
-import { setNotification, removeNotification } from './../reducers/notificationReducer'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { setNotification, removeNotification } from '../reducers/notificationReducer'
 //import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
