c09765385d9c3af938115e49cd8ddee31d6a9c24
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-04-24 09:53:31 +0000

    8.7.5 library-front: setup react-router

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -2,24 +2,33 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 const App = () => {
   const [page, setPage] = useState('authors')
 
   return (
-    <div>
+    <Router>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <nav>
+          <Link to='/authors'>
+            <button>authors</button>
+          </Link>
+          <Link to='/books'>
+            <button>books</button>
+          </Link>
+          <Link to='/add'>
+            <button>add book</button>
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path='/authors' element={<Authors />} />
+          <Route path='/books' element={<Books />} />
+          <Route path='/add' element={<NewBook />} />
+        </Routes>
       </div>
-
-      <Authors show={page === 'authors'} />
-
-      <Books show={page === 'books'} />
-
-      <NewBook show={page === 'add'} />
-    </div>
+    </Router>
   )
 }
 
--- part8/library-frontend/src/components/NewBook.jsx
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
 
-const NewBook = (props) => {
+const NewBook = props => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
-  if (!props.show) {
-    return null
-  }
-
-  const submit = async (event) => {
+  const submit = async event => {
     event.preventDefault()
 
     console.log('add book...')
@@ -33,37 +29,28 @@ const NewBook = (props) => {
       <form onSubmit={submit}>
         <div>
           title
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
+          <input value={title} onChange={({ target }) => setTitle(target.value)} />
         </div>
         <div>
           author
-          <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
+          <input value={author} onChange={({ target }) => setAuthor(target.value)} />
         </div>
         <div>
           published
           <input
-            type="number"
+            type='number'
             value={published}
             onChange={({ target }) => setPublished(target.value)}
           />
         </div>
         <div>
-          <input
-            value={genre}
-            onChange={({ target }) => setGenre(target.value)}
-          />
-          <button onClick={addGenre} type="button">
+          <input value={genre} onChange={({ target }) => setGenre(target.value)} />
+          <button onClick={addGenre} type='button'>
             add genre
           </button>
         </div>
         <div>genres: {genres.join(' ')}</div>
-        <button type="submit">create book</button>
+        <button type='submit'>create book</button>
       </form>
     </div>
   )
