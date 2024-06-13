ec2acaa877ae0602623904246d6d34885ddc25e8
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-06-13 12:46:44 +0000

    8.25 library-front: Subscription updateCache ALL_BOOKS (only all books view/ genre '')

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -7,17 +7,41 @@ import Recommendations from './components/Recommendations'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { useApolloClient, useSubscription } from '@apollo/client'
 import { BOOK_ADDED } from './graphql/booksQl'
+import { ALL_BOOKS } from './graphql/booksQl'
+import { collapseClasses } from '@mui/material'
+
+// function that takes care of manipulating cache
+const updateCache = (cache, query, addedBook) => {
+  // helper that is used to eliminate saving same person twice
+  const uniqByTitle = a => {
+    let seen = new Set()
+    return a.filter(item => {
+      let k = item.title
+      return seen.has(k) ? false : seen.add(k)
+    })
+  }
+  // Genre '' updates in real time only the allbooks cache curerently
+  cache.updateQuery({ ...query, variables: { genre: '' } }, data => {
+    const { allBooks } = data
+    return {
+      allBooks: uniqByTitle(allBooks.concat(addedBook)),
+    }
+  })
+}
 
 const App = () => {
   const [token, setToken] = useState(null)
   const client = useApolloClient()
 
   useSubscription(BOOK_ADDED, {
-    onData: ({ data }) => {
+    onData: ({ data, client }) => {
+      const addedBook = data.data.bookAdded
+      updateCache(client.cache, { query: ALL_BOOKS }, addedBook)
       const title = data.data.bookAdded.title
       window.alert(`New book added: ${title}`)
     },
   })
+
   useEffect(() => {
     const storedToken = localStorage.getItem('token')
     if (storedToken) {
