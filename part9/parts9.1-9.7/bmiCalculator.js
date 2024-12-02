001e0e113d8e92b7e8dd71886832b1cbb1560a91
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2024-12-02 11:47:12 +0000

    9.1 Ts: calculateBmi add types and move to own directory

--- part9/parts9.1-9.7/bmiCalculator.js
+++ part9/parts9.1-9.7/bmiCalculator.js
@@ -1,4 +1,4 @@
-const bmiCategories = [
+var bmiCategories = [
     { max: 16.0, label: 'Underweight (Severe thinness)' },
     { max: 17.0, label: 'Underweight (Moderate thinness)' },
     { max: 18.5, label: 'Underweight (Mild thinness)' },
@@ -8,19 +8,18 @@ const bmiCategories = [
     { max: 40.0, label: 'Obese (Class II)' },
     { max: Infinity, label: 'Obese (Class III)' },
 ];
-
-const calculateBmi = (heightCm: number, weightKg: number): string => {
-    const bmi = (weightKg / ((heightCm/100) * (heightCm/100)));
-    const category = bmiCategories.find((currentItem) => bmi <= currentItem.max);
+var calculateBmi = function (heightCm, weightKg) {
+    var bmi = (weightKg / ((heightCm / 100) * (heightCm / 100)));
+    var category = bmiCategories.find(function (currentItem) { return bmi <= currentItem.max; });
     return category ? category.label : 'Unknown';
 };
-
 try {
-  console.log(calculateBmi(180, 74));
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message;
-  }
-  console.log(errorMessage);
+    console.log(calculateBmi(180, 74));
+}
+catch (error) {
+    var errorMessage = 'Something went wrong: ';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage);
 }
---
+++ part9/parts9.1-9.7/bmiCalculator.ts
@@ -0,0 +1,28 @@
+type BmiCategory = { max: number; label: string };
+
+const bmiCategories = [
+    { max: 16.0, label: 'Underweight (Severe thinness)' },
+    { max: 17.0, label: 'Underweight (Moderate thinness)' },
+    { max: 18.5, label: 'Underweight (Mild thinness)' },
+    { max: 25.0, label: 'Normal range' },
+    { max: 30.0, label: 'Overweight (Pre-obese)' },
+    { max: 35.0, label: 'Obese (Class I)' },
+    { max: 40.0, label: 'Obese (Class II)' },
+    { max: Infinity, label: 'Obese (Class III)' },
+];
+
+const calculateBmi = (heightCm: number, weightKg: number): string => {
+    const bmi = (weightKg / ((heightCm/100) * (heightCm/100)));
+    const category = bmiCategories.find((currentItem: BmiCategory) => bmi <= currentItem.max);
+    return category ? category.label : 'Unknown';
+};
+
+try {
+  console.log(calculateBmi(180, 74));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
